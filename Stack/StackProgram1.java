562a938d0b510a0161e5c24a10d1dd59f90ac481
Devanshu <devanshu2343@gmail.com>
2025-02-12 23:28:54 +0530

    Stack

--- Stack/StackProgram1.java
+++ Stack/StackProgram1.java
@@ -1,10 +1,11 @@
-import java.util.Stack;
-
+import java.util.Stack; 
 public class StackProgram1 {
     public static void main(String[] args) {
         Stack <Integer> st = new Stack<>();
         st.push(4);
         st.push(5);
+        st.pop();
+        System.out.println(st.peek());
         st.push(5);
         st.push(6);
         System.out.println(st.peek()); 
